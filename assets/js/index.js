25bae754d6357949c39c3234de45aeec78631343
GuillinJorge <51805016+GuillinJorge@users.noreply.github.com>
2024-05-06 22:22:28 +0000

    >> Primer POO usando Javascript

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+console.log("Hola mundo")
+
+// Instancia de manera literal
+
+const enmasama = {
+//atributos
+    nombre: "Enma-Sama",
+    cantidadFantasmas: [],  // 
+    reinosOtorgados: {cielo: true, infierno: false},
+    agregarFantasma: function(fantasma){
+        this.cantidadFantasmas.push(fantasma)
+    }
+
+
+// metodos
+
+
+}
